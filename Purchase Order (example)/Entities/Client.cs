1f47ceb94eeb76233f21d47ec399f3af89d16e92
rafavigne <47698747+rafavigne@users.noreply.github.com>
2019-03-16 16:55:20 -0300

    Create Client.cs

---
+++ Purchase Order (example)/Entities/Client.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp9.Entities
+{
+    class Client
+    {
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public Client() { }
+
+        public Client(string name, string email, DateTime birthDate)
+        {
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+        }
+    }
+}
